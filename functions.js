06228b311fe3fe8341c1537b639f7feaedb618ec
Ben Beatty <benbeatty@MOB-BenB.local>
2021-03-15 16:21:33 -0400

    coded up to credit rating

---
+++ functions.js
@@ -0,0 +1,31 @@
+//Price Range Options
+
+$(document).ready(function () {
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 100000,
+        max: 1000000,
+        hide_min_max: true,
+        from: 150000,
+        to: 850000,
+        grid: false,
+        skin: "big",
+        step: 50000,
+        force_edges: true
+    });
+});
+
+
+
+
+// Add active class to the current button (highlight it)
+
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
